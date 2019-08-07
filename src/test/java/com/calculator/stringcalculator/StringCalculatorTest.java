fb0529ff2e8b38de69abd7f3d2f45c918e6226aa
Gabriel <gabomgar@gmail.com>
2019-08-07 16:04:21 -0500

    Agregando gradle

---
+++ src/test/java/com/calculator/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,14 @@
+package com.calculator.stringcalculator;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.*;
+
+public class StringCalculatorTest {
+
+  @Test
+  public void testEmpty(){
+    assertEquals(0, 1);
+  }
+
+}
