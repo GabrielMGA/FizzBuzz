27d44118c59e4aec36f4059ab88b81dc31894f10
Gabriel <gabomgar@gmail.com>
2019-08-07 17:20:18 -0500

    Agregando metodo fizz

--- src/main/java/com/calculator/stringcalculator/DavidDMG12.java
+++ src/main/java/com/calculator/stringcalculator/DavidDMG12.java
@@ -5,4 +5,11 @@ public class DavidDMG12{
     public void Saluda() {
         System.out.println("Hola");
     }
+
+    public String fizz(int number) {
+
+        if (number % 3 == 0)
+            return "Fizz";
+        return "";
+    }
 }
