bba4cd1b0eb7d7d738617dd6dae51026f4fee0bb
Gabriel <gabogap01@gmail.com>
2019-08-07 17:26:33 -0500

    mi clase

--- src/main/java/com/calculator/stringcalculator/ClaseGabrielG.java
+++ src/main/java/com/calculator/stringcalculator/ClaseGabrielG.java
@@ -1,8 +1,13 @@
 package com.calculator.stringcalculator;
 
-public class Garcia {
+public class ClaseGabrielG {
 
-   int x=0;
-   //Garcia
+   for(int i=0;i>=10;i++)
+   {
+      if(i%==3)
+      {
+         Console.writeline("Fizzz");
+      }
+   }
 
 }
