1dae22afc50713bef5a72e88851b0d53240c9979
Armando <armandosanchezhz@gmail.com>
2019-08-07 17:25:23 -0500

    Ejersio del numero divisible  por:Armando

--- src/main/java/com/calculator/stringcalculator/Armando.java
+++ src/main/java/com/calculator/stringcalculator/Armando.java
@@ -1,7 +1,19 @@
 package com.calculator.stringcalculator;
 
-public class ArmandoSan {
-    
+public class GabrielMGA {
 
-    String saludoAr="Armando";
+    public void Saluda() {
+        for(int x=1; x<=50 x++)
+        {
+if(x%3==0)
+{
+
+    System.out.println("Fizz");
+}
+else {
+    System.out.println(x);
+}
+
+        }
+    }
 }
