6c34ce6e1fa8df7681e92d13e6f95324841c0db6
jorge-go <jorge.gerardo018@gmail.com>
2019-08-07 17:51:20 -0500

    Se cambia mensaje de fizz por git

--- src/main/java/com/calculator/stringcalculator/JorgeGO.java
+++ src/main/java/com/calculator/stringcalculator/JorgeGO.java
@@ -12,7 +12,7 @@ public class JorgeGO {
         int num2 =3;
 
         if(num1%num2 ==0){
-            System.out.println("FIZZ");
+            System.out.println("GITTTT");
         }
         else {
             System.out.println("No es DIVISIBLE");
