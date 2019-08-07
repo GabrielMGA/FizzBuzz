9da00a9b035a3c4ef4d7b2f57cd17a8fb105b419
David Martinez <david.martinez@sycpagos.com.mx>
2019-08-07 17:19:38 -0500

    Numero divisible entre 3

--- src/main/java/com/calculator/stringcalculator/DavidDMG12.java
+++ src/main/java/com/calculator/stringcalculator/DavidDMG12.java
@@ -3,6 +3,13 @@ package com.calculator.stringcalculator;
 public class DavidDMG12{
     
     public void Saluda() {
-        System.out.println("Hola");
+
+    	private int num=3;
+
+    	if (num%3==0) {
+
+    		 System.out.println("Fizz");
+    	}
+       
     }
 }
